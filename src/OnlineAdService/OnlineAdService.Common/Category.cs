9f4765c4a09dc34101099a82c073fe3dafaf5d1f
Radu Matei <matei.radu94@gmail.com>
2016-08-04 12:58:55 +0300

    Added common classes - added Ad - added BlobInformation - added Category

---
+++ src/OnlineAdService/OnlineAdService.Common/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineAdService.Common
+{
+    public enum Category
+    {
+        Cars,
+        [Display(Name = "Real Estate")]
+        RealEstate,
+        [Display(Name = "Free Stuff")]
+        FreeStuff
+    }
+}
