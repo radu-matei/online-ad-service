8335b2ac1f48721c30036a66c8f782c9dbe4cf2d
Radu Matei <matei.radu94@gmail.com>
2016-08-04 13:02:21 +0300

    Added initial settings for storage account and queues

--- src/OnlineAdService/OnlineAdService.Web/Global.asax.cs
+++ src/OnlineAdService/OnlineAdService.Web/Global.asax.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System.Configuration;
+using System.Diagnostics;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -16,6 +17,35 @@ namespace OnlineAdService.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            InitializeStorage();
+        }
+
+        private void InitializeStorage()
+        {
+            // Open storage account using credentials from .cscfg file.
+            var storageAccount = CloudStorageAccount.Parse
+                (ConfigurationManager.ConnectionStrings["AzureWebJobsStorage"].ToString());
+
+            Trace.TraceInformation("Creating images blob container");
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var imagesBlobContainer = blobClient.GetContainerReference("images");
+            if (imagesBlobContainer.CreateIfNotExists())
+            {
+                // Enable public access on the newly created "images" container.
+                imagesBlobContainer.SetPermissions(
+                    new BlobContainerPermissions
+                    {
+                        PublicAccess = BlobContainerPublicAccessType.Blob
+                    });
+            }
+
+            Trace.TraceInformation("Creating queues");
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            var blobnameQueue = queueClient.GetQueueReference("thumbnailrequest");
+            blobnameQueue.CreateIfNotExists();
+
+            Trace.TraceInformation("Storage initialized");
         }
     }
 }
