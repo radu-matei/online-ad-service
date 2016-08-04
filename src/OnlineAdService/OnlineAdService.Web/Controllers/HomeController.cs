818dd9656b7e0aa9cc1b99ea6ea1d930ebf68f5a
Radu Matei <matei.radu94@gmail.com>
2016-08-04 12:59:53 +0300

    Updated HomeController with custom titles

--- src/OnlineAdService/OnlineAdService.Web/Controllers/HomeController.cs
+++ src/OnlineAdService/OnlineAdService.Web/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace OnlineAdService.Web.Controllers
 {
@@ -15,14 +11,14 @@ namespace OnlineAdService.Web.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Awesome Online Ad Service";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Us!";
 
             return View();
         }
