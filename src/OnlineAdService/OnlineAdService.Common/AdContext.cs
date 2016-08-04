f00ab8ec2f7d88d1b569955dde91925f14fbe9e7
Radu Matei <matei.radu94@gmail.com>
2016-08-04 12:59:35 +0300

    Added database context class - added Entity Framework context class

---
+++ src/OnlineAdService/OnlineAdService.Common/AdContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace OnlineAdService.Common
+{
+    public class AdContext : DbContext
+    {
+        public AdContext() : base("name=AdContext")
+        {
+        }
+
+        public DbSet<Ad> Ads { get; set; }
+    }
+}
